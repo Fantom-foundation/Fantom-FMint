480c8bcef8a87aaec1a8f709c66497d35ac80525
Jiri Malek <jirka.malek@gmail.com>
2020-08-19 12:30:49 +0200

    Add collateral and debt related error codes.

--- contracts/utils/FMintErrorCodes.sol
+++ contracts/utils/FMintErrorCodes.sol
@@ -6,17 +6,29 @@ pragma solidity ^0.5.0;
  */
 contract FMintErrorCodes {
 	// Error Code: No error.
-	uint256 public const ERR_NO_ERROR = 0x0;
+	uint256 public constant ERR_NO_ERROR = 0x0;
 
 	// Error Code: Zero value is not valid for the call.
-	uint256 public const ERR_INVALID_ZERO_VALUE = 0x1001;
+	uint256 public constant ERR_ZERO_AMOUNT = 0x1001;
 
-	// Error Code: ERC20 account balance too low to continue.
-	uint256 public const ERR_LOW_BALANCE = 0x1002;
+	// Error Code: Account balance too low to continue.
+	uint256 public constant ERR_LOW_BALANCE = 0x1002;
 
 	// Error Code: ERC20 allowance for the fMint too low.
-	uint256 public const ERR_LOW_ALLOWANCE = 0x1003;
+	uint256 public constant ERR_LOW_ALLOWANCE = 0x1003;
 
-	// Error Code: ERC20 collateral is below enforced limit.
-	uint256 public const ERR_LOW_COLLATERAL_RATIO = 0x2001;
+	// Error Code: Collateral is missing or has no value.
+	uint256 public constant ERR_NO_COLLATERAL = 0x1004;
+
+	// Error Code: Collateral is below enforced limit.
+	uint256 public constant ERR_LOW_COLLATERAL_RATIO = 0x1005;
+
+	// Error Code: Requested token not available for minting.
+	uint256 public constant ERR_MINTING_PROHIBITED = 0x1006;
+
+	// Error Code: Requested token has no known value.
+	uint256 public constant ERR_NO_VALUE = 0x1007;
+
+	// Error Code: Requested token has no known value.
+	uint256 public constant ERR_DEBT_EXCEEDED = 0x1007;
 }
