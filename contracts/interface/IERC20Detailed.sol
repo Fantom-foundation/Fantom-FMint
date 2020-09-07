ac24dfd1cd04340ce5c57fe3c1da060c2d3a82b1
Jiri Malek <jirka.malek@gmail.com>
2020-09-07 19:21:32 +0200

    Add interface for ERC20 tokens with detailed setup.
    The token registry expects detailed ERC20 tokens to be used with DeFi protocols so the registry can provide information about the tokens used to client side API interfaces.

---
+++ contracts/interface/IERC20Detailed.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.5.0;
+
+// IERC20Detailed defines the interface to ERC20Detailed token contract.
+// We use this type of contract, @openzeppelin implementation,
+// for our synthetic DeFi ERC20 tokens.
+interface IERC20Detailed {
+    /**
+     * @dev Returns the name of the token.
+     */
+    function name() external view returns (string memory);
+
+    /**
+     * @dev Returns the symbol of the token, usually a shorter version of the
+     * name.
+     */
+    function symbol() external view returns (string memory);
+
+    /**
+     * @dev Returns the number of decimals used to get its user representation.
+     * For example, if `decimals` equals `2`, a balance of `505` tokens should
+     * be displayed to a user as `5,05` (`505 / 10 ** 2`).
+     *
+     * Tokens usually opt for a value of 18, imitating the relationship between
+     * Ether and Wei.
+     *
+     * NOTE: This information is only used for _display_ purposes: it in
+     * no way affects any of the arithmetic of the contract, including
+     * {IERC20-balanceOf} and {IERC20-transfer}.
+     */
+    function decimals() external view returns (uint8);
+}
