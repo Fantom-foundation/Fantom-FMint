664496091af78685a024a3e96b91f80d88e926df
Jiri Malek <jirka.malek@gmail.com>
2020-09-15 19:24:11 +0200

    Add Fantom fUSD token source code.

---
+++ contracts/utility/FantomFUSD.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.5.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
+
+/**
+ * @dev Extension of {ERC20} that allows token holders to destroy both their own
+ * tokens and those that they have an allowance for, in a way that can be
+ * recognized off-chain (via event analysis).
+ */
+contract FantomFUSD is ERC20, ERC20Detailed, ERC20Mintable, ERC20Burnable, ERC20Pausable {
+    // constructor creates the fUSD token
+    constructor(uint256 initialSupply) ERC20Detailed("Fantom USD", "FUSD", 18) public {
+        // nothing to do here
+    }
+}
