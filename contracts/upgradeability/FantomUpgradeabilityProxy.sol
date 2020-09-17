a8bdedc047296619b760ca5ab679f5ffe9654af1
Jiri Malek <jirka.malek@gmail.com>
2020-09-18 01:45:17 +0200

    Add OpenZeppelin upgradeability proxy contract constructor.

--- contracts/upgradeability/FantomUpgradeabilityProxy.sol
+++ contracts/upgradeability/FantomUpgradeabilityProxy.sol
@@ -5,4 +5,9 @@ import "@openzeppelin/upgrades/contracts/upgradeability/AdminUpgradeabilityProxy
 // FantomUpgradeabilityProxy inherits fully from OpenZeppelin Upgradeability Proxy
 // contract with admin access control.
 contract FantomUpgradeabilityProxy is AdminUpgradeabilityProxy {
+    // create the contract instance
+    constructor(address _logic, address _admin, bytes memory _data) AdminUpgradeabilityProxy(_logic, _admin, _data) public payable
+    {
+        // nothing to do here
+    }
 }
