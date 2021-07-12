46c50492935718a2f3f3522af4aef66cc850df31
Light Fury <leech.developer@gmail.com>
2021-07-11 20:43:37 -0600

    - liquidation action

--- contracts/liquidator/FantomLiquidationManager.sol
+++ contracts/liquidator/FantomLiquidationManager.sol
@@ -26,6 +26,8 @@ contract FantomLiquidationManager is Initializable, Ownable
     uint256 public maxAmt;
     uint256 public targetAmt;
 
+    uint256 constant WAD = 10 ** 18;
+
     // initialize initializes the contract properly before the first use.
     function initialize(address owner) public initializer {
         // initialize the Ownable
@@ -55,7 +57,7 @@ contract FantomLiquidationManager is Initializable, Ownable
         (uint256 lockedCollat, uint256 normalisedDebt) = vat.urns(vaultIndex, urn);
         VaultData memory mVaultData = vaultDatas[vaultIndex];
         uint256 dNormalisedDebt;
-        uint256 rate;
+        uint256 rate;   
         uint256 dust;
 
         // dart calculation
@@ -66,7 +68,37 @@ contract FantomLiquidationManager is Initializable, Ownable
         require(maxAmt > targetAmt && mVaultData.maxAmt > mVaultData.targetAmt, "Collateral liquidation limit hit");
         uint256 room = min(maxAmt - targetAmt, mVaultData.maxAmt - mVaultData.targetAmt);
 
-        // dNormalisedDebt = min(normalisedDebt, )
+        dNormalisedDebt = min(normalisedDebt, mul(room, WAD) / rate / mVaultData.liquidationPenalty);
+
+        if (normalisedDebt > dNormalisedDebt) {
+            if (mul(normalisedDebt - dNormalisedDebt, rate) < dust) {
+                dNormalisedDebt = normalisedDebt;
+            } else {
+                require(mul(dNormalisedDebt, normalisedDebt) >= dust, "Dusty auction from partial liquidation");
+            }
+        }
+
+        uint256 dink = mul(lockedCollat, dNormalisedDebt) / normlaisedDebt;
+
+        require(dink > 0, "Null auction");
+        require(dNormalisedDebt <= 2**255 && dink <= 2**255, "Overflow");
+
+        // Grab the collateral
+        // vat.grab(ilk, urn, mVaultData.liquidator, address(vow), -int256(dink), -uint256(dNormalisedDebt));
+
+        uint256 due = mul(dNormalisedDebt, rate);
+        vow.fess(due);
+
+        uint256 tab = mul(due, mVaultData.liquidationPenalty) / WAD;
+        targetAmt = add(targetAmt, tab);
+
+        vaultDatas[vaultIndex].targetAmt = add(mVaultData.targetAmt, tab);
+        FantomAuctionManager(mVaultData.liquidation).kick({
+            tab: tab,
+            lot: dink,
+            usr: urn,
+            kpr: rewardReceiver
+        });
     }
 
     function endLiquidation() external auth {
